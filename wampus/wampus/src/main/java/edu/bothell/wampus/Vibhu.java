e69b23e16b225772c1c7c943da1901a298c56e10
Vibhu Rampally <2011243@apps.nsd.org>
2025-02-11 14:47:24 -0800

    vibhu first

--- wampus/wampus/src/main/java/edu/bothell/wampus/Vibhu.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Vibhu.java
@@ -1,12 +1,14 @@
 package edu.bothell.wampus;
 
 public class Vibhu {
+    // Properties
+    private String name = "vibhu"
     
     public Vibhu(){
         System.out.println("Hello");
     }
 
     public void Hello(){
-        System.out.println("Hi im neel");
+        System.out.println("Hi im vibhu");
     }
 }
