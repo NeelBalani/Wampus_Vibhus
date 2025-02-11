e5351d8d8f4a99ba56115ffe6756a9443455c094
Vibhu Rampally <2011243@apps.nsd.org>
2025-02-11 15:09:40 -0800

    hw in Person

--- wampus/wampus/src/main/java/edu/bothell/wampus/Person.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Person.java
@@ -2,4 +2,10 @@ package edu.bothell.wampus;
 
 public class Person{
 
+// Should take the property array list of strings
+// Strings should be the name of the classes that we make in wumpus
+
+// In a new class called main, create a new instance of itself
+// Create a new Person class
+
 }
