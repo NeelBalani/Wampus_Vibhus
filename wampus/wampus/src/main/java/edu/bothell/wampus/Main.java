2f8d3b68c6a32534cc382defb270887dac77c48d
Pranav Mullakkal <1108132@apps.nsd.org>
2025-02-13 13:54:23 -0800

    changes 1:54 PM 2/13

--- wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Main.java
@@ -6,6 +6,8 @@ public class Main{
     public static void main(String[] args){
         Neel neel = new Neel();
         neel.getJobs();
+        Pranav pranav = new Pranav();
+        pranav.getJobs();
     } 
 
 }
