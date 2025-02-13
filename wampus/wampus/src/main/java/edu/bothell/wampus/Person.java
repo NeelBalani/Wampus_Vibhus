77ccf10f2b7bf8fc5dab6b9e1507db9f58bc2a00
vibhu <vibhu.rampally@gmail.com>
2025-02-12 21:13:00 -0800

    i was just looking

--- wampus/wampus/src/main/java/edu/bothell/wampus/Person.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Person.java
@@ -6,9 +6,4 @@ public class Person{
 
     ArrayList<String> jobs = new ArrayList<String>();
 
-// Should take the property array list of strings
-// Strings should be the name of the classes that we make in wumpus
-
-// In a new class called main, create a new instance of itself
-// Create a new Person class
 }
