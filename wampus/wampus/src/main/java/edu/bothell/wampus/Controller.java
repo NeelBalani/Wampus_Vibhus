97ff47926c76e7a6e360205b43b7e0ee9555d647
pranavmullakkal <1108132@apps.nsd.org>
2025-02-14 14:37:24 -0800

    pranav 237214

--- wampus/wampus/src/main/java/edu/bothell/wampus/Controller.java
+++ wampus/wampus/src/main/java/edu/bothell/wampus/Controller.java
@@ -26,7 +26,6 @@ public class Controller {
             ui.showPersonTurn((Person) activeTeammate);
             Result result = activeTeammate.doAction(ui);
             ui.showMessage(result.getMessage());
-
             continueGame = ui.askToContinue(activeTeammate.getName());
             if(!continueGame){
                 removePlayer(activeTeammate);
